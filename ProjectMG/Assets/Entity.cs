92bd3cebfdbae5ddc2e3dec7584c2e96bc29c0ea
MINUTE1084 <swpro1004@gmail.com>
2022-05-06 02:43:10 +0900

    PlayerController 수정

--- ProjectMG/Assets/Entity.cs
+++ ProjectMG/Assets/Entity.cs
@@ -1,18 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float maxHP;
+    private float maxMP;
 
-    // Update is called once per frame
-    void Update()
+    private float HP;
+    private float MP;
+
+    private List<string> items;
+    private List<string> skills;
+
+    private void Start()
     {
-        
     }
 }
--- ProjectMG/Assets/PlayerController.cs
+++ ProjectMG/Assets/PlayerController.cs
@@ -4,35 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
- Rigidbody2D rigid2D;
- float jumpForce = 800.0f;
- float walkSpeed = 7.0f;
- // Start is called before the first frame update
- void Start()
- {
-  this.rigid2D = GetComponent<Rigidbody2D>();
- }
+    Rigidbody2D rigid2D;
+    public float jumpForce = 800.0f;
+    public float walkSpeed = 7.0f;
+    public float maxVelocity = 7.0f;
 
- // Update is called once per frame
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
 
- private void Update()
- {
-  if (Input.GetKeyDown(KeyCode.Space) && rigid2D.velocity.y == 0)
-  {
-   Debug.Log("Jump");
-   this.rigid2D.AddForce(transform.up * this.jumpForce);
-  }
+    private void Update()
+    {
+        Vector2 moveVelocity = new Vector2(
+            Input.GetAxisRaw("Horizontal") * walkSpeed, 
+            Input.GetKeyDown(KeyCode.Space) && rigid2D.velocity.y == 0 ? jumpForce : 0);
+        rigid2D.AddForce(moveVelocity);
+    }
 
-  Vector3 moveVelocity = Vector3.zero;
-  if (Input.GetKey(KeyCode.RightArrow))
-  {
-   moveVelocity = Vector3.right;
-  }
-  if (Input.GetKey(KeyCode.LeftArrow))
-  {
-   moveVelocity = Vector3.left;
-  }
-  transform.position += moveVelocity * walkSpeed * Time.deltaTime;
-
- }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = new Vector2(rigid2D.velocity.x, 0);
+        if (velocity.sqrMagnitude > (maxVelocity * maxVelocity)) rigid2D.velocity = new Vector2(rigid2D.velocity.normalized.x * maxVelocity, rigid2D.velocity.y);
+    }
 }
